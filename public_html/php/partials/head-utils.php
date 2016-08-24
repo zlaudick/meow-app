19824f2d4f477c3c171f037601fc842db388fa8b
Zac Laudick <zlaudick@nmsu.edu>
2016-08-24 10:44:24 -0600

    linked angular files in head-utils updated links in nav

--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -6,6 +6,9 @@
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+		<!-- set base for relative links - to enable pretty URLs -->
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/";?>">
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -25,6 +28,14 @@
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-route.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-animate.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.3.3/ui-bootstrap-tpls.min.js"></script>
+
+		<!-- load our angular files -->
+		<script src="angular/meow-app.js"></script>
+		<script src="angular/route-config.js"></script>
+		<script src="angular/controllers/home-controller.js"></script>
+		<script src="angular/controllers/donate-controller.js"></script>
+		<script src="angular/controllers/signup-controller.js"></script>
+		<script src="angular/controllers/treats-controller.js"></script>
 
 		<title>MeowApp v1.5.0</title>
 	</head>
--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -9,17 +9,17 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/">MeowApp v1.5.0</a>
+				<a class="navbar-brand" href="index">MeowApp v1.5.0</a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/">Home</a></li>
-					<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/treats">Treats</a></li>
-					<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/donate">Donate</a></li>
-					<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/sign-up">Sign Up</a></li>
+					<li><a href="index">Home</a></li>
+					<li><a href="treats">Treats</a></li>
+					<li><a href="donate">Donate</a></li>
+					<li><a href="sign-up">Sign Up</a></li>
 					<li><a href="https://github.com/rlewis2892/meow-app" target="_blank">GitHub</a></li>
 				</ul>
 			</div><!-- /.navbar-collapse -->
