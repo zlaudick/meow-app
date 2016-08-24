f3658f6cbd136ecaae91e73b3f950c7c7f4d0edd
Zac Laudick <zlaudick@nmsu.edu>
2016-08-24 11:34:24 -0600

    final changes to navbar

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,9 +1,13 @@
-<header>
+<header ng-controller="navController">
+
+	<!-- bootstrap breakpoint directive to control collapse behavior -->
+	<bootstrap-breakpoint></bootstrap-breakpoint>
+
 	<nav class="navbar navbar-default navbar-inverse">
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<button type="button" class="navbar-toggle collapsed" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -13,7 +17,7 @@
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse" uib-collapse="navCollapsed">
 
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="index">Home</a></li>
