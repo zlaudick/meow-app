63e4d10223f46dfb9930e628ad604e3aacdc91c6
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-08-23 18:04:23 -0600

    create static layout - ready for live demo

---
+++ public_html/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+
+		<!-- Our Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<title>MeowApp v1.5.0</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<!--begin header-->
+			<header>
+				<nav class="navbar navbar-default navbar-inverse">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/">MeowApp v1.5.0</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/">Home</a></li>
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/treats">Treats</a></li>
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/donate">Donate</a></li>
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37/meow-app/public_html/sign-up">Sign Up</a></li>
+								<li><a href="https://github.com/rlewis2892/meow-app" target="_blank">GitHub</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<!--begin main content -->
+			<main>
+				<div class="container-fluid">
+
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="jumbotron text-right">
+								<h1><i class="fa fa-paw"></i> MeowApp v1.5.0</h1>
+								<p class="lead">Modernize Feeding Times. Mandate More Naps.</p>
+								<a href="sign-up" class="btn btn-warning btn-lg">Join Us!</a>
+								<a href="#" class="btn btn-warning btn-lg">Log In</a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</main>
+		</div>
+
+		<!--begin footer -->
+		<footer>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12">
+						&copy;&nbsp;2016 Romulan Star Empire
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
